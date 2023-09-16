b93f9f10b1da1397fb33f7ffa0fead8da5c1114a
Omar Hany <OmarMohamed20150362@fci.helwan.edu.eg>
2023-09-16 19:12:10 +0300

    Adding data provider

--- src/test/java/base/BaseTests.java
+++ src/test/java/base/BaseTests.java
@@ -22,6 +22,7 @@ public class BaseTests {
         homePage = new HomePage(driver);
         driver.get(readDataFromJson.readJsonFile().URL);
 
+
     }
 
     @AfterClass
--- src/test/java/login/LoginTests.java
+++ src/test/java/login/LoginTests.java
@@ -1,20 +1,37 @@
 package login;
 
 import base.BaseTests;
+import org.json.simple.parser.ParseException;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import pages.LoginPage;
-import reader.ReadDataFromJson;
+import reader.ReadDataDrivenFromJson;
 
-import java.io.FileNotFoundException;
+import java.io.IOException;
 
 public class LoginTests extends BaseTests {
+    ReadDataDrivenFromJson readDataDrivenFromJson;
 
-    @Test
-    public void testSuccessfulLogin() throws FileNotFoundException {
-        readDataFromJson = new ReadDataFromJson();
-        LoginPage loginPage = homePage.clickOnLoginLink();
-        loginPage.insertUsername(readDataFromJson.readJsonFile().Login.ValidCredentials.Username);
-        loginPage.insertPassword(readDataFromJson.readJsonFile().Login.ValidCredentials.Password);
-        loginPage.clickOnLoginButton();
+    @DataProvider
+    public Object[] testDataForSuccessfulLogin() throws IOException, ParseException {
+        readDataDrivenFromJson = new ReadDataDrivenFromJson();
+        return readDataDrivenFromJson.testDataForSuccessfulLogin();
     }
+
+    // Omar,123
+    // Mohamed,456
+
+
+    @Test(dataProvider = "testDataForSuccessfulLogin")
+    public void t(String data){
+        String users[]=data.split(",");
+
+        LoginPage loginPage =homePage.clickOnLoginLink();
+        loginPage.insertUsername(users[0]);
+        loginPage.insertPassword(users[1]);
+    }
+
+
+
+
 }
